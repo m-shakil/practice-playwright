71d9b546bb12b1c099a467235c344d574246916b
m-shakil <m.shakil@gmail.com>
2026-06-10 16:00:17 +0600

    Refactor alert handling and basic auth tests for improved clarity and structure

--- tests/alertHandling.spec.js
+++ tests/alertHandling.spec.js
@@ -11,7 +11,9 @@ test('successful message should be displayed after alert handling', async ({ pag
   await page.locator('[href="/javascript_alerts"]').click();
 
   // accept alert
-  
+  page.on('dialog', dialog => dialog.accept());
+  await page.locator('[onclick="jsAlert()"]').click();
+
   // expect that successfulMessage is displayed
-  
+  await expect(page.locator('#result')).toHaveText(successfulMessage);
 });
--- tests/basicAuth.spec.js
+++ tests/basicAuth.spec.js
@@ -4,15 +4,22 @@ import { test, expect } from '@playwright/test';
 const successfulMessage = 'Congratulations! You must have the proper credentials.';
 
 // Set httpCredentials
-
+test.use({
+  httpCredentials: {
+    username: 'admin',
+    password: 'admin'
+  }
+});
 
 test.beforeEach(async ({ page }) => {
   await page.goto('https://the-internet.herokuapp.com');
+  // await page.goto('https://admin:admin@the-internet.herokuapp.com/basic_auth');
 });
 
 test('basic auth works', async ({ page }) => {
   await page.locator('[href="/basic_auth"]').click();
   
   // Verify that successfulMessage is displayed
-
+  const message = await page.locator('//*[@class="example"]//p').innerText();
+  expect(message).toEqual(successfulMessage);
 });
